995c48f3be730485f13389fb40635b328979ac23
jonas.moeneclaey <jonas.moeneclaey@student.staam.be>
2025-03-26 11:43:21 +0100

    eerste push

--- script/script.js
+++ script/script.js
@@ -21,3 +21,28 @@ const users = [
 ];
 
 // hieronder komt jullie code - veel succes
+const fullNameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const registreerBtn = document.querySelector("#registreer")
+const registreerForm = document.querySelector("#registreerForm")
+
+
+
+registreerBtn.addEventListener("click", () => {
+  if (fullNameInput.value == '' || emailInput.value == '' || passwordInput.value == '') {
+  const foutMelding
+}
+  else{
+  users.push({fullname: fullNameInput.value, email: emailInput.value, password: passwordInput.value})
+  console.log(users);
+}})
+
+
+const lijstGebruikers = document.querySelector("#lijstGebruikers")
+const spanUsername = document.querySelector("#username")
+const spanEmail = document.querySelector("#email")
+spanUsername.innerHTML= ""
+spanEmail.innerHTML= ""
+
+
